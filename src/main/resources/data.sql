b155f83f392286809a5816358003d7eb48602bde
Jean <jeansoaresferreira72@gmail.com>
2023-11-06 10:02:14 -0300

    service

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO TASK (title, type, description, score, status) VALUES ('Carga', 'Treino', 'Progredir Carga', 50, 30);
-INSERT INTO TASK (title, type, description, score) VALUES ('Dieta', 'Alimentacao', 'Bater os macros e micros nutrientes diarios', 40);
-INSERT INTO TASK (title, type, description, score) VALUES ('Dormir', 'Descanso', 'Dormir de 7h a 9h Diarias', 60);
+INSERT INTO TASK (title, type, description, score, status) VALUES ('Dieta', 'Alimentacao', 'Bater os macros e micros nutrientes diarios', 40);
+INSERT INTO TASK (title, type, description, score, status) VALUES ('Dormir', 'Descanso', 'Dormir de 7h a 9h Diarias', 60);
